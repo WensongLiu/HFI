23d20ada58384ac0ee84d4d2650cf5e3c721a649
Wensong Liu <lws.19940712@gmail.com>
2019-08-21 06:15:05 +0800

    add static files

--- DjangoDemo/django_demo/django_demo/urls.py
+++ DjangoDemo/django_demo/django_demo/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('yellow/', views.yellow),
     path('welcomePage/', views.welcomePage),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
