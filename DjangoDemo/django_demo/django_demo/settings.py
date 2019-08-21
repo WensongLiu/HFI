3ac3c22271029e21bda296e742763d14c0573d95
Wensong Liu <lws.19940712@gmail.com>
2019-08-22 06:06:24 +0800

    remove password

--- DjangoDemo/django_demo/django_demo/settings.py
+++ DjangoDemo/django_demo/django_demo/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_for_django',
         'USER': 'root',
-        'PASSWORD': '712918Lwslbs',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
